2b44308f13a0fb4610a517dceeed4c4ca691a178
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:18:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,50 @@
+// // part 19 // question 23// conditional test
+
+let car = `subaru`;
+
+// // $ true tests
+
+// // equality test
+// console.log("Is car ==  `subaru`? I predict true,")
+// console.log(car == `subaru`); // true
+
+// // strict equality test
+// console.log("Is car === `subaru`? I predict true,")
+// console.log(car === `subaru`); // true
+
+// // unequality test
+// console.log("Is car != `toyota`? I predict true,")
+// console.log(car != `toyota`); // true
+
+// // strict unequality test
+// console.log("Is car !== `toyota`? I predict true,")
+// console.log(car !== `toyota`); // true
+
+// // equality test
+// console.log("Is car ==  `subaru`? I predict true,")
+// console.log(car == `subaru`); // true
+
+
+
+// // equality test
+console.log("Is car ==  `subaru`? I predict false,")
+console.log(car == `subaru`); // false
+
+
+
+
+// // equality test
+ console.log("Is car == undefined? I predict false,")
+console.log(car == `undefined`); // false
+
+// // strict equality test
+console.log("Is car === `toyota`? I predict false,")
+ console.log(car === `toyota`); // false
+
+// // strict equality test
+console.log("Is car === null? I predict false,")
+ console.log(car === null); // false
+
+// // unequality test
+ console.log("Is car != `subaru`? I predict false,")
+console.log(car != `subaru`); // false
